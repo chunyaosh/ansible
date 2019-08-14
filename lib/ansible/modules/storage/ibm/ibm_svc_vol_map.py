ab04a474fd4830c339baa356ee8480b9a657b838
chunyao <chunyao@cn.ibm.com>
2019-08-14 22:43:09 +0800

    Trivial: document for ibm_svc_mdiskgrp.py ibm_svc_vdisk.py           ibm_svc_vol_map.py

--- lib/ansible/modules/storage/ibm/ibm_svc_vol_map.py
+++ lib/ansible/modules/storage/ibm/ibm_svc_vol_map.py
@@ -147,7 +147,7 @@ class IBMSVCvdiskhostmap(object):
 
         data = self.restapi.svc_obj_info(cmd='lsvdiskhostmap', cmdopts=None, cmdargs=[self.volname])
 
-        if type(data) is list:
+        if isinstance(data, list):
             for d in data:
                 merged_result.update(d)
         else:
